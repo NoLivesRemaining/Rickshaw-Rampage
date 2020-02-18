f4f7764756c8c29a238925cb420ca30c38cdcc03
Stephen Wann <c16409712@mydit.ie>
2020-02-18 12:38:59 +0000

    StephenScene Updated

---
+++ Rickshaw Rampage/Assets/Script/SpeedPowerup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerup : MonoBehaviour
+{
+    private MeshRenderer Mesh;
+    private BoxCollider BCollider;
+
+    public bool isBoosted = false;
+    public float respawnTimer = 0f;
+
+    void Start()
+    {
+        Mesh = GetComponent<MeshRenderer>();
+        BCollider = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        respawnTimer++;
+
+        if (respawnTimer >= 1000f)
+        {
+            RespawnPickup();
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            isBoosted = true;
+            Invoke("SetBoolBack", 5);
+            Mesh.enabled = false;
+            BCollider.enabled = false;
+            respawnTimer = 0f;
+        }
+    }
+
+    void RespawnPickup()
+    {
+        Mesh.enabled = true;
+        BCollider.enabled = true;
+    }
+
+    void SetBoolBack()
+    {
+        isBoosted = false;
+    }
+}
